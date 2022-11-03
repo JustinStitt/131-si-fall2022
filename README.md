548f088bf32cc39f1dcc28a7344a90f8e402eab7
Justin Stitt <cpt.crixus@gmail.com>
2022-11-03 13:58:29 -0700

    add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Fall 2022] CPSC 131 SI
+
+A collection of code-based resources!
+
+[worksheets](https://drive.google.com/drive/u/0/folders/1YTJpIMszCcYVKQ2DmBSkM9iSZZezSJR9)
--- test.py
+++ test.py
@@ -3,18 +3,8 @@ print("hi")
 
 def bruh(a, b):
     print("asdfasdf")
-    sersdf
     return 1 + 2 + 3 + 4 + 5 + 6 + 7
 
 
 def addTwo(a, b):
-    print("hello, world")
-    print("asdasdad")V
-    print("asdasd")
-    print("asdasdasd")
-    print("asdasdasd")
-
-
-
-
-
+    return a + b
